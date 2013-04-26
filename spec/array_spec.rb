4418ffd23e187e2fbdb20a3e7839e9271ac2b829
Michael Hartl <michael@michaelhartl.com>
2013-04-26 12:31:27 -0700

    Reorganize specs

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe Array do
-  it "can be created with no arguments" do
-    expect(Array.new).to be_instance_of(Array)
-  end
 
-  # it "can be created with a specific size" do
-  #   Array.new(9).size.must_equal 10
-  # end
+  describe '#new' do
+    it "should work with no arguments" do
+      expect(Array.new).to be_instance_of(Array)
+    end
+
+    it "should work with a numerical argument" do
+      expect(Array.new(10).size).to eq(10)
+    end
+  end
 end
