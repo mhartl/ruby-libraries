98b390e53d21c7c3d7870464e69ce9ed94fd1908
Michael Hartl <michael@michaelhartl.com>
2013-04-29 17:12:00 -0700

    Add more array tests

--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -3,12 +3,44 @@ require 'spec_helper'
 describe Array do
 
   describe '#new' do
-    it "should work with no arguments" do
-      expect(Array.new).to be_instance_of(Array)
+
+    context "when called with no arguments" do
+      it "should return the empty array" do
+        expect(Array.new).to eq []
+      end
     end
 
-    it "should work with a numerical argument" do
-      expect(Array.new(10).size).to eq(10)
+    context "when called with an integer as an argument" do
+      let(:array) { Array.new(3) }
+
+      it "should have length equal to the argument" do
+        expect(array.length).to eq 3
+      end
+
+      it "should return an array with all nils" do
+        expect(array).to eq [nil, nil, nil] 
+      end
+    end
+
+    context "when called with an integer and a value" do
+      let(:array) { Array.new(3, 'foo') }
+
+      it "should have length equal to the first argument" do
+        expect(array.length).to eq 3
+      end
+
+      it "should return an array consisting of the given value" do
+        expect(array).to eq ['foo', 'foo', 'foo']
+      end
+    end
+  end
+
+  describe "ary & other_ary" do
+    let(:ary) { [1, 1, 3, 5, 'foo'] }
+    let(:other_ary) { [1, 2, 3, 'foo'] }
+
+    it "should return the set intersection of the two arrays" do
+      expect(ary & other_ary).to eq [1, 3, 'foo']
     end
   end
 end
